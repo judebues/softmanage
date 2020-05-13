ce1d4af25b8defc15829020c8e4c765653ae437e
yml <908553683@qq.com>
2020-05-13 21:04:32 +0800

    增加了注意事项，使用插件话技术开发

--- work/yml/Wordprocessing/views.py
+++ work/yml/Wordprocessing/views.py
@@ -9,10 +9,12 @@ from rest_framework import viewsets
 def home_page(request):
     result = Commdisaster.objects.values()
     # print(type(result))
-    print(result)
     # result_list = [ item for item in result]
     return render(request,'index.html',{'info':result})
 
+# search info according the request
+def search_info(request):
+    
 
 class CommdisasterViewSet(viewsets.ModelViewSet):
     queryset = Commdisaster.objects.all()
